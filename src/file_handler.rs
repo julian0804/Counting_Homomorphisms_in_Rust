1dac43677824f3ead8300e92cf1dcdf8bde64a32
s7587383@stud.uni-frankfurt.de <Sp|:~W;ztNh$#6mUN*|vy$/c4fBysB>
2022-03-06 18:03:16 +0100

    updated file_handler.rs - added more comments

--- src/file_handler.rs
+++ src/file_handler.rs
@@ -23,7 +23,7 @@ pub mod file_handler {
     /*
     creates nice tree decomposition from file
     Does not check if the tree decomposition is correct
-    TODO: make code more solid by catching errors of wrong formation
+    TODO: make code more solid by catching errors of wrong formatting
      */
     pub fn create_ntd_from_file<P>(filename: P) -> Option<NiceTreeDecomposition>
         where P: AsRef<Path>
