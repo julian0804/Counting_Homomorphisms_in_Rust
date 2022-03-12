af24182fdd13dd163960d06057bea83dc8e6cfe9
s7587383@stud.uni-frankfurt.de <Sp|:~W;ztNh$#6mUN*|vy$/c4fBysB>
2022-03-12 17:25:34 +0100

    minor changes

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,6 @@ use crate::graph_structures::graph_structures::graph::SimpleGraph;
 use crate::graph_structures::graph_structures::nice_tree_decomposition::NiceTreeDecomposition;
 
 fn main(){
-    let ntd = file_handler::file_handler::create_ntd_from_file("example_2.ntd").unwrap();
-    println!("{:?}", ntd.stingy_ordering());
+
+
 }
