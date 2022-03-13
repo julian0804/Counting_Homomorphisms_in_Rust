640336d7d3ce80d79a12b14a4bb864483f4c58b8
s7587383@stud.uni-frankfurt.de <Sp|:~W;ztNh$#6mUN*|vy$/c4fBysB>
2022-03-13 11:12:09 +0100

    added crates for testing

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,13 @@ mod file_handler;
 mod graph_structures;
 mod algorithms;
 
+use petgraph::dot::{Dot, Config};
+use petgraph::graph::NodeIndex;
+use petgraph::matrix_graph::*;
+use petgraph::matrix_graph::MatrixGraph;
 use crate::file_handler::file_handler::*;
-use crate::graph_structures::graph_structures::graph::SimpleGraph;
 use crate::graph_structures::graph_structures::nice_tree_decomposition::NiceTreeDecomposition;
 
 fn main(){
-
 
 }
