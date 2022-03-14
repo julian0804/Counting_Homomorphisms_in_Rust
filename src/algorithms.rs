2ccb945d87dba6cc4cfd222bf7bfc00d213a2000
s7587383@stud.uni-frankfurt.de <Sp|:~W;ztNh$#6mUN*|vy$/c4fBysB>
2022-03-14 14:56:18 +0100

    Added test cases for - DPData::new - DPData::set - DPData::get

--- src/algorithms.rs
+++ src/algorithms.rs
@@ -16,7 +16,7 @@ pub mod diaz {
     /*
     a struct containing all infos about the dynamic program
      */
-    struct DPData {
+    pub struct DPData {
         table : HashMap<TreeNode, HashMap<Mapping, u64>>,
         from_graph : MatrixGraph<(),(), Undirected>,
         to_graph : MatrixGraph<(),(), Undirected>,
@@ -25,7 +25,7 @@ pub mod diaz {
 
 
     impl DPData {
-        pub fn new(from_graph : SimpleGraph, to_graph : SimpleGraph) -> DPData {
+        pub fn new(from_graph : MatrixGraph<(),(), Undirected>, to_graph : MatrixGraph<(),(), Undirected>) -> DPData {
             DPData { table : HashMap::new(), from_graph, to_graph }
         }
 
@@ -56,8 +56,8 @@ pub mod diaz {
 
     1. Use Hashmaps for representing the mappings
      */
-
-    pub fn diaz(from_graph : SimpleGraph, ntd : NiceTreeDecomposition, to_graph : SimpleGraph) -> u64
+    /*
+    pub fn diaz(from_graph : MatrixGraph<(),(), Undirected>, ntd : NiceTreeDecomposition, to_graph : MatrixGraph<(),(), Undirected>) -> u64
     {
         let stingy_order = ntd.stingy_ordering();
 
@@ -93,15 +93,29 @@ pub mod diaz {
         1
     }
 
-
+ */
 }
+
+
 
 #[cfg(test)]
 mod tests{
+    use crate::algorithms::diaz::DPData;
+    use crate::file_handler::file_handler::metis_to_graph;
 
     #[test]
     fn test_dpdata(){
+        let from_graph = metis_to_graph("data/metis_graphs/from_2.graph").unwrap();
+        let to_graph = metis_to_graph("data/metis_graphs/to_2.graph").unwrap();
+        let mut test_dp_data = DPData::new(from_graph, to_graph);
 
+        test_dp_data.set(5,4,4);
+        test_dp_data.set(2,3,5);
+        test_dp_data.set(9,12,3);
+
+        assert_eq!(*test_dp_data.get(&5,&4).unwrap(), 4);
+        assert_eq!(*test_dp_data.get(&2,&3).unwrap(), 5);
+        assert_eq!(*test_dp_data.get(&9,&12).unwrap(), 3);
     }
 
     #[test]
