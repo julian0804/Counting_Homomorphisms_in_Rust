91468f634b6953645ef718dcecc6ae2974362f39
s7587383@stud.uni-frankfurt.de <Sp|:~W;ztNh$#6mUN*|vy$/c4fBysB>
2022-04-12 08:56:09 +0200

    Testing

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,25 @@ mod file_handler;
 mod graph_structures;
 mod algorithms;
 
+use std::collections::HashSet;
 use petgraph::dot::{Dot, Config};
 use petgraph::graph::NodeIndex;
 use petgraph::matrix_graph::*;
 use petgraph::matrix_graph::MatrixGraph;
+use crate::algorithms::diaz::diaz;
 
 fn main(){
+
+    let from_graph = file_handler::file_handler::metis_to_graph("data/metis_graphs/from_2.graph").unwrap();
+    let to_graph = file_handler::file_handler::metis_to_graph("data/metis_graphs/to_2.graph").unwrap();
+    let ntd = file_handler::file_handler::create_ntd_from_file("data/nice_tree_decompositions/example_2.ntd").unwrap();
+    let i = diaz(&from_graph, ntd, &to_graph);
+    println!("{:?}", i);
+
+    let from_graph = file_handler::file_handler::metis_to_graph("data/metis_graphs/from_3.graph").unwrap();
+    let to_graph = file_handler::file_handler::metis_to_graph("data/metis_graphs/to_3.graph").unwrap();
+    let ntd = file_handler::file_handler::create_ntd_from_file("data/nice_tree_decompositions/example_2.ntd").unwrap();
+    let i = diaz(&from_graph, ntd, &to_graph);
+    println!("{:?}", i);
 
 }
