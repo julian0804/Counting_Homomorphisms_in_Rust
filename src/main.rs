1ed710c3bb945e6a3b4c68f0da6f2c271e5a6976
s7587383@stud.uni-frankfurt.de <Sp|:~W;ztNh$#6mUN*|vy$/c4fBysB>
2022-04-21 13:45:44 +0200

    just some testing

--- src/main.rs
+++ src/main.rs
@@ -10,11 +10,23 @@ use petgraph::dot::{Dot, Config};
 use petgraph::graph::NodeIndex;
 use petgraph::matrix_graph::*;
 use petgraph::matrix_graph::MatrixGraph;
-use crate::algorithms::diaz::{diaz, generate_edges};
+use crate::algorithms::diaz::{diaz, generate_edges, generate_graphs};
+use std::time::{Duration, Instant};
 
 fn main(){
 
-    let ntd = file_handler::file_handler::create_ntd_from_file("data/nice_tree_decompositions/example_2.ntd").unwrap();
-    println!("{:?}", generate_edges(ntd));
+    let ntd = file_handler::file_handler::create_ntd_from_file("data/nice_tree_decompositions/example_3.ntd").unwrap();
+    let g = file_handler::file_handler::metis_to_graph("data/metis_graphs/to_2.graph").unwrap();
 
+
+    let start = Instant::now();
+    let graphs = generate_graphs(generate_edges(ntd.clone()));
+
+
+    for h in &graphs{
+        //println!("{:?}", Dot::new(g));
+        println!("{:?}", diaz(h,ntd.clone(), &g));
+    }
+    let duration = start.elapsed();
+    println!("{:?} homomorphism numbers have been calculated in time {:?}", graphs.len(), duration);
 }
