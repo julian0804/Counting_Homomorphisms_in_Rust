8e8093b777d290a1a62c48fb452dc200fd43e460
s7587383@stud.uni-frankfurt.de <Sp|:~W;ztNh$#6mUN*|vy$/c4fBysB>
2022-05-11 18:52:54 +0200

    set modules to public

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,10 @@
 extern crate core;
 
-mod tree_decompositions;
-mod file_handler;
+pub mod tree_decompositions;
+pub mod file_handler;
 mod unit_tests;
-mod brute_force;
-mod diaz;
-mod equivalence_class_algorithm;
-mod integer_functions;
-mod graph_generation;
+pub mod brute_force;
+pub mod diaz;
+pub mod equivalence_class_algorithm;
+pub mod integer_functions;
+pub mod graph_generation;
