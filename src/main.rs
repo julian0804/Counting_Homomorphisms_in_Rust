f6c4b672b63d9066c368f81d0e6d8b3c0c5df7ab
s7587383@stud.uni-frankfurt.de <Sp|:~W;ztNh$#6mUN*|vy$/c4fBysB>
2022-06-10 16:26:41 +0200

    added all experiments

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,48 @@
 extern crate core;
 
 use std::path::Path;
-use Counting_Homomorphisms::experiments::single_running_time_measurement::{run_experiment};
+use Counting_Homomorphisms::brute_force::brute_force_homomorphism_counter::simple_brute_force_for_ntd_set;
+use Counting_Homomorphisms::diaz_serna_thilikos::diaz_algorithm::diaz_serna_thilikos_for_ntd_set;
+use Counting_Homomorphisms::experiments::single_running_time_measurement::{measure_running_time};
+use Counting_Homomorphisms::modified_DP::algorithm::modified_dp;
+use Counting_Homomorphisms::modified_dp::algorithm::modified_dp;
 
 fn main(){
-    run_experiment(Path::new("data/Experiments/runtime.csv"));
+
+    // measure single running times
+
+    measure_running_time(Path::new("data/Experiments/experiment_matrices/running_time/brute_force_growth_with_e_tau.csv"),
+                         simple_brute_force_for_ntd_set,
+                         &"brute_force".to_string());
+
+    measure_running_time(Path::new("data/Experiments/experiment_matrices/running_time/brute_force_growth_with_graph.csv"),
+                         simple_brute_force_for_ntd_set,
+                         &"brute_force".to_string());
+
+    measure_running_time(Path::new("data/Experiments/experiment_matrices/running_time/diaz_serna_thilikos_growth_with_e_tau.csv"),
+                         diaz_serna_thilikos_for_ntd_set,
+                         &"diaz_serna_thilikos".to_string());
+
+    measure_running_time(Path::new("data/Experiments/experiment_matrices/running_time/diaz_serna_thilikos_growth_with_graph.csv"),
+                         diaz_serna_thilikos_for_ntd_set,
+                         &"diaz_serna_thilikos".to_string());
+
+    measure_running_time(Path::new("data/Experiments/experiment_matrices/running_time/modified_dp_growth_with_e_tau.csv"),
+                         modified_dp,
+                         &"modified_dp".to_string());
+
+
+    measure_running_time(Path::new("data/Experiments/experiment_matrices/running_time/modified_dp_growth_with_graph.csv"),
+                         modified_dp,
+                         &"modified_dp".to_string());
+
+    measure_running_time(Path::new("data/Experiments/experiment_matrices/running_time/mixed_combinations.csv"),
+                         diaz_serna_thilikos_for_ntd_set,
+                         &"diaz_serna_thilikos".to_string());
+
+
+    measure_running_time(Path::new("data/Experiments/experiment_matrices/running_time/mixed_combinations.csv"),
+                         modified_dp,
+                         &"modified_dp".to_string());
+
 }
