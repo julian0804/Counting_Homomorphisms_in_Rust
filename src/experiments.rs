2e5eb5125084cba880aba885656d307e8b551426
s7587383@stud.uni-frankfurt.de <Sp|:~W;ztNh$#6mUN*|vy$/c4fBysB>
2022-06-10 16:23:27 +0200

    minor stylistic changes

--- src/experiments.rs
+++ src/experiments.rs
@@ -7,40 +7,19 @@ pub mod single_running_time_measurement {
     use std::time::{Duration, Instant};
     use csv;
     use itertools::Itertools;
+    use petgraph::matrix_graph::MatrixGraph;
+    use petgraph::Undirected;
+    use crate::brute_force::brute_force_homomorphism_counter::simple_brute_force_for_ntd_set;
     use crate::diaz_serna_thilikos::diaz_algorithm::diaz_serna_thilikos_for_ntd_set;
-    use crate::modified_DP::algorithm::equivalence_class_algorithm;
+    use crate::modified_dp::algorithm::modified_dp;
     use crate::file_handler::graph_handler::import_metis;
     use crate::file_handler::tree_decomposition_handler::import_ntd;
     use crate::graph_generation::graph_generation_algorithms::generate_possible_edges;
+    use crate::tree_decompositions::nice_tree_decomposition::NiceTreeDecomposition;
 
-    /// a function getting all paths of tree decomposition
-    fn get_nice_tree_decomposition_paths() -> ReadDir
-    {
-        let ntd_path = Path::new("./data/nice_tree_decompositions/benchmark_ntds");
-        fs::read_dir(ntd_path).unwrap()
-    }
-
-    /// A function getting all paths of path ntds
-    fn get_path_ntd_paths() -> ReadDir
-    {
-        let ntd_path = Path::new("./data/nice_tree_decompositions/benchmark_ntds/path_ntds");
-        fs::read_dir(ntd_path).unwrap()
-    }
-
-    /// A function getting all paths of path ntds
-    fn get_complete_ntd_paths() -> ReadDir
-    {
-        let ntd_path = Path::new("./data/nice_tree_decompositions/benchmark_ntds/complete_ntds");
-        fs::read_dir(ntd_path).unwrap()
-    }
-
-    /// a function returning all paths of tree decomposition
-    fn get_auto_generated_graph_paths() -> ReadDir
-    {
-        let graph_path = Path::new("./data/metis_graphs/auto_generated_graphs");
-        fs::read_dir(graph_path).unwrap()
-    }
-
+    const RESULT_PATH: &str = "./target/experiment_results/";
+    const NTD_PATH: &str = "data/Experiments/ntds/";
+    const GRAPH_PATH: &str = "data/Experiments/graphs/";
 
     /// lists necessary information of the tree decomposition and write them into a csv file
     pub fn list_ntd_data() {
@@ -81,33 +60,28 @@ pub mod single_running_time_measurement {
         }
     }
 
-    /// This function imports a experiment matrix and measures the marked combinations
-    pub fn run_experiment(matrix_file: &Path) {
+    /// This methods executes the experiment given by matrix_path with the algorithm alg and the name alg_name
+    pub fn measure_running_time(matrix_file : &Path, alg : fn(&NiceTreeDecomposition, &MatrixGraph<(), (), Undirected>) -> Vec<(MatrixGraph<(), (), Undirected>, u64)>, alg_name : &String){
 
-        // Construct file path of output file
-        let result_path = "./target/experiment_results/";
-        let date = chrono::Local::now().timestamp().to_string();
+        let test_name = matrix_file.file_stem().unwrap().to_str().unwrap();
 
-        let name = matrix_file.file_name().unwrap().to_str().unwrap();
-
-        let filepath = format!("{}experiment_{}_results.csv", result_path, name);
+        // Setting output path
+        let filepath = format!("{}{}_{}_results.csv", RESULT_PATH, alg_name, test_name);
         let filepath = Path::new(&filepath);
 
-
-        // fixing the experiment paths
-        let ntd_path = "./data/nice_tree_decompositions/benchmark_ntds/final_selection/";
-        let graph_path = "./data/metis_graphs/final_selection/";
-
+        // Reading experiment matrix
         let mut reader = csv::Reader::from_path(matrix_file).unwrap();
-
         let headers = reader.headers().unwrap().clone();
+
+        println!("###### Running time experiment for {} ####", alg_name);
+
 
         // iterates over all ntd
         for record in reader.records() {
             let record = record.unwrap();
             let ntd_name = &record[0];
 
-            let single_ntd_path = format!("{}{}", ntd_path, ntd_name);
+            let single_ntd_path = format!("{}{}", NTD_PATH, ntd_name);
             let single_ntd_path = Path::new(&single_ntd_path);
 
             // iterate over all graphs
@@ -116,7 +90,7 @@ pub mod single_running_time_measurement {
                 if u == 0 || v.parse::<u32>().unwrap() == 0 { continue; }
 
                 let graph_name = &headers[u];
-                let single_graph_path = format!("{}{}", graph_path, graph_name);
+                let single_graph_path = format!("{}{}", GRAPH_PATH, graph_name);
                 let single_graph_path = Path::new(&single_graph_path);
 
                 let ntd = import_ntd(single_ntd_path).unwrap();
@@ -140,70 +114,45 @@ pub mod single_running_time_measurement {
                 let v_g = graph.node_count();
                 let e_g = graph.edge_count();
 
-
-
                 //Equivalence class algorithm
-                let mut measurements_equivalence_algorithm = vec![];
-                println!("Equivalence Algorithm: running experiment for ntd {:?} and graph {:?}", ntd_name, graph_name);
+                let mut measurements = vec![];
+                println!("Running experiment for ntd {:?} and graph {:?}", ntd_name, graph_name);
 
                 for i in 0..5 {
                     println!("running test number {}", i + 1);
                     let start = Instant::now();
 
-                    equivalence_class_algorithm(&ntd, &graph);
+                    alg(&ntd, &graph);
 
                     let duration = start.elapsed();
                     println!("time needed: {:?}", duration);
-                    measurements_equivalence_algorithm.push(duration);
+                    measurements.push(duration);
                 }
 
-                let sum: Duration = measurements_equivalence_algorithm.iter().sum();
-                let avg_equivalence_algorithm = sum.div_f32(measurements_equivalence_algorithm.len() as f32);
-                println!("average running time is {:?}", avg_equivalence_algorithm);
-
-                println!("Diaz: running experiment for ntd {:?} and graph {:?}", ntd_name, graph_name);
-                let mut measurements_diaz = vec![];
-
-                for i in 0..5 {
-                    println!("running test number {}", i + 1);
-                    let start = Instant::now();
-
-                    diaz_serna_thilikos_for_ntd_set(&ntd, &graph);
-
-                    let duration = start.elapsed();
-                    println!("time needed: {:?}", duration);
-                    measurements_diaz.push(duration);
-                }
-
-                let sum: Duration = measurements_diaz.iter().sum();
-                let avg_diaz = sum.div_f32(measurements_diaz.len() as f32);
-                println!("average running time is {:?}", avg_diaz);
+                let sum: Duration = measurements.iter().sum();
+                let avg_measurements = sum.div_f32(measurements.len() as f32);
+                println!("average running time is {:?}", avg_measurements);
 
                 wtr.write_record(&[
-                    "equivalence algorithm",
-                    &ntd_name,
+                    &alg_name,
+                    &ntd_name.to_string(),
                     &width.to_string(),
                     &v_t.to_string(),
                     &e_tau.to_string(),
                     &v_tau.to_string(),
-                    &graph_name,
+                    &graph_name.to_string(),
                     &v_g.to_string(),
                     &e_g.to_string(),
-                    &measurements_equivalence_algorithm[0].as_millis().to_string(),
-                    &measurements_equivalence_algorithm[1].as_millis().to_string(),
-                    &measurements_equivalence_algorithm[2].as_millis().to_string(),
-                    &measurements_equivalence_algorithm[3].as_millis().to_string(),
-                    &measurements_equivalence_algorithm[4].as_millis().to_string(),
-                    &avg_equivalence_algorithm.as_micros().to_string(),
-                    &measurements_diaz[0].as_millis().to_string(),
-                    &measurements_diaz[1].as_millis().to_string(),
-                    &measurements_diaz[2].as_millis().to_string(),
-                    &measurements_diaz[3].as_millis().to_string(),
-                    &measurements_diaz[4].as_millis().to_string(),
-                    &avg_diaz.as_micros().to_string(),
+                    &measurements[0].as_micros().to_string(),
+                    &measurements[1].as_micros().to_string(),
+                    &measurements[2].as_micros().to_string(),
+                    &measurements[3].as_micros().to_string(),
+                    &measurements[4].as_micros().to_string(),
+                    &avg_measurements.as_micros().to_string(),
                 ]
                 );
             }
         }
+
     }
 }
