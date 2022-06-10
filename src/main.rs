b3b92e9eb9225d8e13d6644239b46ca621cd8f7c
s7587383@stud.uni-frankfurt.de <Sp|:~W;ztNh$#6mUN*|vy$/c4fBysB>
2022-06-10 16:51:58 +0200

    added experiments to main.rs

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,11 @@ use std::path::Path;
 use Counting_Homomorphisms::brute_force::brute_force_homomorphism_counter::simple_brute_force_for_ntd_set;
 use Counting_Homomorphisms::diaz_serna_thilikos::diaz_algorithm::diaz_serna_thilikos_for_ntd_set;
 use Counting_Homomorphisms::experiments::single_running_time_measurement::{measure_running_time};
-use Counting_Homomorphisms::modified_DP::algorithm::modified_dp;
 use Counting_Homomorphisms::modified_dp::algorithm::modified_dp;
 
 fn main(){
 
     // measure single running times
-
     measure_running_time(Path::new("data/Experiments/experiment_matrices/running_time/brute_force_growth_with_e_tau.csv"),
                          simple_brute_force_for_ntd_set,
                          &"brute_force".to_string());
@@ -32,9 +30,13 @@ fn main(){
                          &"modified_dp".to_string());
 
 
+
     measure_running_time(Path::new("data/Experiments/experiment_matrices/running_time/modified_dp_growth_with_graph.csv"),
                          modified_dp,
                          &"modified_dp".to_string());
+
+
+    // new measurements
 
     measure_running_time(Path::new("data/Experiments/experiment_matrices/running_time/mixed_combinations.csv"),
                          diaz_serna_thilikos_for_ntd_set,
@@ -44,5 +46,8 @@ fn main(){
     measure_running_time(Path::new("data/Experiments/experiment_matrices/running_time/mixed_combinations.csv"),
                          modified_dp,
                          &"modified_dp".to_string());
+
+
+    // Comparison
 
 }
