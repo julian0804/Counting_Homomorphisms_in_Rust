994c49faa355297eecf44e374fdf2937d2ba072d
s7587383@stud.uni-frankfurt.de <Sp|:~W;ztNh$#6mUN*|vy$/c4fBysB>
2022-06-10 16:57:20 +0200

    renamed module modified_dp

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@ pub mod file_handler;
 mod unit_tests;
 pub mod brute_force;
 pub mod diaz_serna_thilikos;
-pub mod modified_DP;
+pub mod modified_dp;
 pub mod integer_functions;
 pub mod graph_generation;
 pub mod experiments;
